bef7c721a3734c5d25f4a640545d0f54bdb10cef
Prince-Arora <19princearora95@gmail.com>
2017-12-31 02:58:20 +0530

    code

--- app/src/main/java/com/example/laptop/otp_using_firebase/MainActivity.java
+++ app/src/main/java/com/example/laptop/otp_using_firebase/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.laptop.otp_using_firebase;
 
-import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -20,8 +20,9 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    //FirebaseApp.initializeApp(this);//run krke dekho ayega
+    private FirebaseAuth Auth;
     EditText Phone, otp;
-    FirebaseAuth Auth;
     String verificationCode;
     PhoneAuthProvider.OnVerificationStateChangedCallbacks mcallback;
 
@@ -29,25 +30,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Phone = (EditText) findViewById(R.id.editText1);
         otp = (EditText) findViewById(R.id.editText2);
         Auth = FirebaseAuth.getInstance();
+
         mcallback = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
             public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
-
+                Log.d("saurabh", phoneAuthCredential.toString());
             }
 
             @Override
             public void onVerificationFailed(FirebaseException e) {
-
+                Log.d("saurabh2", e.toString());
             }
 
             @Override
             public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
                 super.onCodeSent(s, forceResendingToken);
-                verificationCode=s;
-                Toast.makeText(getApplicationContext(),"Code sent to number",Toast.LENGTH_SHORT).show();
+                verificationCode = s;
+                Log.d("saurabh","Code sent to number");
+                Toast.makeText(getApplicationContext(), "Code sent to number", Toast.LENGTH_SHORT).show();
             }
 
         };
@@ -63,23 +67,22 @@ public class MainActivity extends AppCompatActivity {
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                   if(task.isSuccessful())
-                   {
-                       Toast.makeText(getApplicationContext(),"signed In successfully",Toast.LENGTH_SHORT).show();
-                   }
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "signed In successfully", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
 
     }
-    public void verify(View v)
-    {
-        String inputcode=otp.getText().toString();
-        if(verificationCode.equals(""))
-        veryfycode(verificationCode,inputcode);
+
+    public void verify(View v) {
+        String inputcode = otp.getText().toString();
+        if (verificationCode.equals(""))
+            veryfycode(verificationCode, inputcode);
     }
-public void veryfycode(String verificationCode,String inputCode)
-{
-    PhoneAuthCredential credential=PhoneAuthProvider.getCredential(verificationCode,inputCode);
-signInwithPhone(credential);
-}
+
+    public void veryfycode(String verificationCode, String inputCode) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCode, inputCode);
+        signInwithPhone(credential);
+    }
 }
